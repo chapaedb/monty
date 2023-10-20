36487d7d930e2ea7cee16d9a14d99b73b172ac00
root <chapaedb>
2023-10-20 07:11:26 -0700

    Done

--- push.c
+++ push.c
@@ -9,20 +9,24 @@ void f_push(stack_t **head, unsigned int number)
 {
     int num, index = 0, flag = 0;
 
-    if (bus.value)
+    if (bus.value != NULL && bus.value[0] != '\0')
     {
         if (bus.value[0] == '-')
             index++;
         for (; bus.value[index] != '\0'; index++)
         {
             if (!isdigit(bus.value[index]))
+            {
                 flag = 1;
+                break; // Exit the loop if a non-digit character is encountered
+            }
         }
         if (flag == 1)
         {
             fprintf(stderr, "L%d: usage: push integer\n", number);
             fclose(bus.file_ptr);
             free(bus.content_ptr);
+            // Free the stack
             free_stack(*head);
             exit(EXIT_FAILURE);
         }
@@ -32,6 +36,7 @@ void f_push(stack_t **head, unsigned int number)
         fprintf(stderr, "L%d: usage: push integer\n", number);
         fclose(bus.file_ptr);
         free(bus.content_ptr);
+        // Free the stack
         free_stack(*head);
         exit(EXIT_FAILURE);
     }
@@ -41,5 +46,5 @@ void f_push(stack_t **head, unsigned int number)
     if (bus.flag == 0)
         addnode(head, num);
     else
-	    addqueue(head, num);
+        addqueue(head, num);
 }
