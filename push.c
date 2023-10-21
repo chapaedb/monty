7dcd8fd901b26601c51a03cf33af5131133627fa
root <chapaedb>
2023-10-21 05:34:22 -0700

    DOne

--- push.c
+++ push.c
@@ -1,47 +1,37 @@
 #include "monty.h"
-
 /**
- * f_push - Pushes an element to the stack
- * @head: Double pointer to the head of the stack
- * @number: Line number of the opcode
- */
-void f_push(stack_t **head, unsigned int number)
+ * f_push - add node to the stack
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+*/
+void f_push(stack_t **head, unsigned int counter)
 {
-    int num, index = 0, flag = 0;
-    
-    if (bus.value)
-{
-for (index = 0; bus.value[index] != '\0'; index++)
-    {
-        if (bus.value[index] < '0' || bus.value[index] > '9')
-        {
-            flag = 1;
-            break;
-        }
-    }	
-    if (flag == 1)
-    {
-        fprintf(stderr, "L%d: usage: push integer\n", number);
-        if (bus.file_ptr != NULL)
-            fclose(bus.file_ptr);
-        free(bus.content_ptr);
-        free_stack(*head);
-        exit(EXIT_FAILURE);
-    }
-}
-else
-{
-    fprintf(stderr, "L%d: usage: push integer\n", number);
-    if (bus.file_ptr != NULL)
-        fclose(bus.file_ptr);
-    free(bus.content_ptr);
-    free_stack(*head);
-    exit(EXIT_FAILURE);
-}
-    num = atoi(bus.value);
+        int n, index = 0, flag = 0;
 
-    if (bus.flag == 0)
-        addnode(head, num);
-    else
-            addqueue(head, num);
+        if (bus.value)
+        {
+                if (bus.value[0] == '-')
+                        index++;
+                for (; bus.value[index] != '\0'; index++)
+                {
+                        if (bus.value[index] > 57 || bus.value[index] < 48)
+                                flag = 1; }
+                if (flag == 1)
+                { fprintf(stderr, "L%d: usage: push integer\n", counter);
+                        fclose(bus.file_ptr);
+                        free(bus.content_ptr);
+                        free_stack(*head);
+                        exit(EXIT_FAILURE); }}
+        else
+        { fprintf(stderr, "L%d: usage: push integer\n", counter);
+                fclose(bus.file_ptr);
+                free(bus.content_ptr);
+                free_stack(*head);
+                exit(EXIT_FAILURE); }
+        n = atoi(bus.value);
+        if (bus.flag == 0)
+                addnode(head, n);
+        else
+                addqueue(head, n);
 }
